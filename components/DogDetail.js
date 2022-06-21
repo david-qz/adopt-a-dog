155a3d294a1fa5ee162a756e902a0bc2890f17d7
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-21 15:56:47 -0700

    Implement DogDetail component display

--- components/DogDetail.js
+++ components/DogDetail.js
@@ -6,11 +6,9 @@ export default function createDogDetail(root) {
     const description = root.querySelector('.description');
 
     return ({ dog }) => {
-        // *** use the dog data to complete the following:
-        img.src = ''; // get the image based on breed from folder ../assets/dogs/
-        img.alt = ''; // use the dog's name as the alt
-        // format a string like "Googoo is a 2 year old beagle":
-        intro.textContent = '';
-        description.textContent = ''; // dog description
+        img.src = `/assets/dogs/${dog.breed}.jpeg`; // get the image based on breed from folder ../assets/dogs/
+        img.alt = dog.name; // use the dog's name as the alt
+        intro.textContent = `${dog.name} is a ${dog.age} year old ${dog.breed}.`;
+        description.textContent = dog.description;
     };
 }
